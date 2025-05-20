e440b0235932f6ac73552c4c6d83647adbbd8f1c
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2025-05-20 10:59:22 +0530

    made cors changes

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   exposedHeaders: ["set-cookie"],
 };
